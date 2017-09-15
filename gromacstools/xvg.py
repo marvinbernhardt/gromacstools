c7dbf212f546de866072ebf177ecaf7e9b632e1d
marvinbernhardt <marvinbernhardt@users.noreply.github.com>
2017-09-15 16:57:52 +0200

    more flexible xvg plotting

--- gromacstools/xvg.py
+++ gromacstools/xvg.py
@@ -4,31 +4,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from io import StringIO
 
-def plot(filename):
-    """plots a grace file. Does not work with all grace features!"""
-    data, header = load(filename)
+
+def plot_data_header(data, header):
+    """plots data and header. Does not work with all grace features!"""
     data = np.array(data)
 
     if header['type'].lower() == 'xy':
-        plt.figure(figsize=(10,3))
+        plt.figure(figsize=(10, 3))
         for column in range(len(data[0]) - 1):
-            plt.plot(data[:,0], data[:,column + 1], label=header['legend'][column + 1])
+            plt.plot(data[:, 0], data[:, column + 1],
+                     label=header['legend'][column + 1])
         plt.xlabel(header['xlabel'])
         plt.ylabel(header['ylabel'])
         plt.legend()
         plt.show()
 
     elif header['type'].lower() == 'xydy':
-        plt.figure(figsize=(10,3))
-        plt.errorbar(data[:,0], data[:,1], yerr=data[:,2], label=header['ylabel'])
+        plt.figure(figsize=(10, 3))
+        plt.errorbar(data[:, 0], data[:, 1], yerr=data[:, 2],
+                     label=header['ylabel'])
         plt.xlabel(header['xlabel'])
         plt.ylabel(header['ylabel'])
         plt.legend()
         plt.show()
 
 
+def plot(filename):
+    """plots a grace file. Does not work with all grace features!"""
+    data, header = load(filename)
+    plot_data_header(data, header)
+
+
 def load(filename):
-    """parses a grace file and returns its data and header info. Does not work with all grace features!"""
+    """parses a grace file and returns its data and header info.
+    Does not work with all grace features!"""
     grace_types_legend = {
         'XY': ['x', 'y'],
         'XYDX': ['x', 'y', 'dx'],
@@ -61,7 +70,8 @@ def load(filename):
                 parts = shlex.split(line)
                 if parts[0] == "@TYPE":
                     header['type'] = parts[1]
-                    header['legend'] = grace_types_legend[header['type'].upper()]
+                    header['legend'] = \
+                        grace_types_legend[header['type'].upper()]
                 elif parts[1] == "title" and parts[2].startswith('"'):
                     header['title'] = ' '.join(parts[2:])
                 elif parts[1:3] == ["xaxis", "label"]:
@@ -69,7 +79,7 @@ def load(filename):
                 elif parts[1:3] == ["yaxis", "label"]:
                     header['ylabel'] = ' '.join(parts[3:])
                 elif parts[1][0] == 's' and parts[2] == "legend":
-                    column = int(parts[1][1:]) + 1 # grace does not count x column
+                    column = int(parts[1][1:]) + 1  # grace does not count x
                     if column < len(header['legend']):
                         header['legend'][column] = ' '.join(parts[3:])
                     else:
