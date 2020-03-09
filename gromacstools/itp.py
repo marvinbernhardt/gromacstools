95726a7e2d70731db68cfd40832ab62ff87dad8d
marvinbernhardt <marvinbernhardt@users.noreply.github.com>
2020-03-09 22:58:20 +0100

    reworked itp

--- gromacstools/itp.py
+++ gromacstools/itp.py
@@ -1,4 +1,4 @@
-import configparser
+import re
 from itertools import combinations
 from itertools import permutations
 
@@ -10,125 +10,190 @@ itp files.
 """
 
 
-def get_bonds(itp_file):
-    config = configparser.ConfigParser(
-        allow_no_value=True, inline_comment_prefixes=[";"]
-    )
-    config.optionxform = str
-    config.read(itp_file)
+def get_section_lines(filepath, section):
+    """
+    Parse itp file for lines of a section
 
-    try:
-        bonds_keys = config[" bonds "].keys()
-    except KeyError:
-        bonds_keys = []
+    Parameters
+    ----------
+    filepath : str
+        Filepath for file to be parsed
+    section : str
+        section to parse for
+
+    Returns
+    -------
+    lines : list
+        list of lines
+
+    """
+    _reg_section = re.compile(r'\s*\[\s*([a-zA-Z0-9-_]+)\s*\]\s*')
+    _reg_comment = re.compile(r'\s*;')
+    _reg_empty = re.compile(r'\s*' + '\n')
+
+    section_lines = []
+    with open(filepath, 'r') as f:
+        lines = f.readlines()
+
+        cur_section = None
+        for line in lines:
+
+            # if comment or empty
+            if _reg_comment.match(line) or _reg_empty.match(line):
+                continue
+
+            # if section header
+            match_section = _reg_section.match(line)
+            if match_section:
+                cur_section = match_section.group(1)
+                continue
+
+            # if in wanted section
+            if cur_section == section:
+                section_lines.append(line)
+    return section_lines
+
+
+def get_bonds(filepath):
+    """
+    Parse itp file for bonds
+
+    Parameters
+    ----------
+    filepath : str
+        Filepath for file to be parsed
+
+    Returns
+    -------
+    bonds : set
+
+    """
+    _reg_bond = re.compile(r'\s*([0-9]+)\s*([0-9]+)\s*([0-9]+)')
+
+    bonds_lines = get_section_lines(filepath, 'bonds')
 
     bonds = set()
-    for line in bonds_keys:
-        # bond is a set of {atom0, atom1}
-        bond = frozenset(map(int, line.strip().split()[0:2]))
+    for line in bonds_lines:
+        match_bond = _reg_bond.match(line)
+        if match_bond is None:
+            raise Exception('unable to parse line into bond:', line)
+        ai, aj, func = map(int, match_bond.group(1, 2, 3))
+        bond = frozenset({ai, aj})
         bonds.add(bond)
-
     return bonds
 
 
-def get_pairs(itp_file):
-    config = configparser.ConfigParser(
-        allow_no_value=True, inline_comment_prefixes=[";"]
-    )
-    config.optionxform = str
-    config.read(itp_file)
+def get_pairs(filepath):
+    """
+    Parse itp file for pairs
 
-    try:
-        pairs_keys = config[" pairs "].keys()
-    except KeyError:
-        pairs_keys = []
+    Parameters
+    ----------
+    filepath : str
+        Filepath for file to be parsed
+
+    Returns
+    -------
+    pairs : set
+
+    """
+    _reg_pair = re.compile(r'\s*([0-9]+)\s*([0-9]+)\s*([0-9]+)')
+
+    pairs_lines = get_section_lines(filepath, 'pairs')
 
     pairs = set()
-    for line in pairs_keys:
-        # pair is a set of {atom0, atom1}
-        pair = frozenset(map(int, line.strip().split()[0:2]))
+    for line in pairs_lines:
+        match_pair = _reg_pair.match(line)
+        if match_pair is None:
+            raise Exception('unable to parse line into pair:', line)
+        ai, aj, func = map(int, match_pair.group(1, 2, 3))
+        pair = frozenset({ai, aj})
         pairs.add(pair)
-
     return pairs
 
 
-def get_angles(itp_file):
-    config = configparser.ConfigParser(
-        allow_no_value=True, inline_comment_prefixes=[";"]
-    )
-    config.optionxform = str
-    config.read(itp_file)
+def get_angles(filepath):
+    """
+    Parse itp file for angles
 
-    try:
-        angles_keys = config[" angles "].keys()
-    except KeyError:
-        angles_keys = []
+    Parameters
+    ----------
+    filepath : str
+        Filepath for file to be parsed
+
+    Returns
+    -------
+    angles : set
+
+    """
+    _reg_angle = re.compile(r'\s*([0-9]+)\s*([0-9]+)\s*([0-9]+)\s*([0-9]+)')
+
+    angles_lines = get_section_lines(filepath, 'angles')
 
     angles = set()
-    for line in angles_keys:
-        atoms = list(map(int, line.strip().split()[0:3]))
+    for line in angles_lines:
+        match_angle = _reg_angle.match(line)
+        if match_angle is None:
+            raise Exception('unable to parse line into angle:', line)
         # angle is an tuple of (middle_atom, set of {outer_atom1, outer_atom2})
-        angle = (atoms[1], frozenset({atoms[0], atoms[2]}))
+        ai, aj, ak, func = map(int, match_angle.group(1, 2, 3, 4))
+        angle = (aj, frozenset({ai, ak}))
         angles.add(angle)
-
     return angles
 
 
-def get_dihedrals(itp_file):
-    config = configparser.ConfigParser(
-        allow_no_value=True, inline_comment_prefixes=[";"]
-    )
-    config.optionxform = str
-    config.read(itp_file)
+def get_dihedrals(filepath, ignore_funcs=(2, 4)):
+    """
+    Parse itp file for dihedrals
 
-    try:
-        dihedrals_keys = config[" dihedrals "].keys()
-    except KeyError:
-        dihedrals_keys = []
+    Parameters
+    ----------
+    filepath : str
+        Filepath for file to be parsed
+    ignore_funcs : tuple
+        Dihedral funcs to be ignored. Default is (2, 4) which are improper
+        dihedrals in Gromacs.
+
+    Returns
+    -------
+    dihedrals : set
+
+    """
+    _reg_dihedral = re.compile(r'\s*([0-9]+)\s*([0-9]+)'
+                               + r'\s*([0-9]+)\s*([0-9]+)\s*([0-9]+)')
+
+    dihedrals_lines = get_section_lines(filepath, 'dihedrals')
 
     dihedrals = set()
-    for line in dihedrals_keys:
-        atoms = list(map(int, line.strip().split()[0:4]))
+    for line in dihedrals_lines:
+        match_dihedral = _reg_dihedral.match(line)
+        if match_dihedral is None:
+            raise Exception('unable to parse line into dihedral:', line)
+        ai, aj, ak, al, func = map(int, match_dihedral.group(1, 2, 3, 4, 5))
+        # exclude type 2 and 4 which are improper dihedrals
+        if func in ignore_funcs:
+            continue
         # dihedral is a set of {tuple of (inner_atom, outer_atom), tuple of
         # (inner_atom, outer_atom)}
-        dihedral = frozenset({(atoms[1], atoms[0]), (atoms[2], atoms[3])})
+        dihedral = frozenset({(aj, ai), (ak, al)})
         dihedrals.add(dihedral)
-
     return dihedrals
 
 
-def pairs_from_bonds(bonds, distance=3):
-    pairs = set()
-    if distance == 3:
-        # iterate over all combinations of two bonds
-        for dihedral in dihedrals_from_bonds(bonds):
-            dihedral_iter = iter(dihedral)
-            pair = frozenset({next(dihedral_iter)[1], next(dihedral_iter)[1]})
-            pairs.add(pair)
-    else:
-        raise Exception(f"Pair list for distance of {distance} not implemented yet")
-
-    return pairs
-
-
-def angles_from_bonds(bonds):
-    angles = set()
-    # iterate over all combinations of two bonds
-    for bond_combo in combinations(bonds, 2):
-        bond0 = bond_combo[0]
-        bond1 = bond_combo[1]
-        common_atom = bond0.intersection(bond1)
-        assert len(common_atom) < 2  # double bond?
-
-        # if they have common atom
-        if len(common_atom) == 1:
-            angle = (next(iter(common_atom)), bond0 - common_atom | bond1 - common_atom)
-            angles.add(angle)
-
-    return angles
-
-
 def dihedrals_from_bonds(bonds):
+    """
+    Generate dihedrals from bonds
+
+    Parameters
+    ----------
+    bonds : set
+        bonds describing a molecule
+
+    Returns
+    -------
+    dihedrals : set
+
+    """
     dihedrals = set()
     # iterate over all combinations of three bonds
     for bond_combo in permutations(bonds, 3):
@@ -140,7 +205,8 @@ def dihedrals_from_bonds(bonds):
             | bond1.intersection(bond2)
             | bond2.intersection(bond0)
         )
-        assert len(common_atoms) < 3  # cycle of 3 atoms or double bond?
+        if len(common_atoms) == 3:  # cycle of 3 atoms
+            continue
 
         # if they have two common atoms
         if len(common_atoms) == 2:
@@ -162,95 +228,85 @@ def dihedrals_from_bonds(bonds):
     return dihedrals
 
 
-def angles_lines_from_angles(angles):
+def pairs_from_bonds(bonds, distance=3):
+    """
+    Generate pairs from bonds
+
+    Parameters
+    ----------
+    bonds : set
+        bonds describing a molecule
+    distance : int
+        distance (in bonds) the pairs should have
+
+    Returns
+    -------
+    pairs : set
+
+    """
+    pairs = set()
+    if distance == 3:
+        # iterate over all combinations of two bonds
+        for dihedral in dihedrals_from_bonds(bonds):
+            dihedral_iter = iter(dihedral)
+            pair = frozenset({next(dihedral_iter)[1], next(dihedral_iter)[1]})
+            pairs.add(pair)
+    else:
+        raise Exception(f"Pair list for distance of {distance} not implemented yet")
+
+    return pairs
+
+
+def angles_from_bonds(bonds):
+    """
+    Generate angles from bonds
+
+    Parameters
+    ----------
+    bonds : set
+        bonds describing a molecule
+
+    Returns
+    -------
+    angles : set
+
+    """
+    angles = set()
+    # iterate over all combinations of two bonds
+    for bond_combo in combinations(bonds, 2):
+        bond0 = bond_combo[0]
+        bond1 = bond_combo[1]
+        common_atom = bond0.intersection(bond1)
+        assert len(common_atom) < 2  # double bond?
+
+        # if they have common atom
+        if len(common_atom) == 1:
+            angle = (next(iter(common_atom)), bond0 - common_atom | bond1 - common_atom)
+            angles.add(angle)
+
+    return angles
+
+
+def angles_lines_from_angles(angles, func=1):
+    """
+    Generate angles lines for a topology file
+
+    Parameters
+    ----------
+    angles : set
+
+    Returns
+    -------
+    angles_lines : str
+    """
 
     angles_lines = []
 
     for angle in angles:
         inner_atom = angle[0]
         outer_atoms = list(angle[1])
-        angle_line = f"{outer_atoms[0]:2d} {inner_atom:2d} {outer_atoms[1]:2d} 1"
+        angle_line = f"{outer_atoms[0]:2d} {inner_atom:2d} {outer_atoms[1]:2d}  {func}"
         angles_lines.append(angle_line)
 
     angles_lines = "\n".join(angles_lines)
     return angles_lines
-
-
-def set_bonds(itp_file, bonds, bond_type="1"):
-    config = configparser.ConfigParser(
-        allow_no_value=True, inline_comment_prefixes=["#", ";"]
-    )
-    config.optionxform = str
-    config.read(itp_file)
-
-    config[" bonds "] = {}
-    for bond in bonds:
-        atoms = list(bond)
-        bond_line = f"{atoms[0]:<2d} {atoms[1]:<2d}  {bond_type}"
-        config[" bonds "][bond_line] = None
-
-    with open(itp_file, "w") as fp:
-        config.write(fp)
-
-
-def set_pairs(itp_file, pairs, pair_type="1"):
-    config = configparser.ConfigParser(
-        allow_no_value=True, inline_comment_prefixes=["#", ";"]
-    )
-    config.optionxform = str
-    config.read(itp_file)
-
-    config[" pairs "] = {}
-    for pair in pairs:
-        atoms = list(pair)
-        pair_line = f"{atoms[0]:<2d} {atoms[1]:<2d}  {pair_type}"
-        config[" pairs "][pair_line] = None
-
-    with open(itp_file, "w") as fp:
-        config.write(fp)
-
-
-def set_angles(itp_file, angles, angle_type="1"):
-    config = configparser.ConfigParser(
-        allow_no_value=True, inline_comment_prefixes=["#", ";"]
-    )
-    config.optionxform = str
-    config.read(itp_file)
-
-    config[" angles "] = {}
-    for angle in angles:
-        inner_atom = angle[0]
-        outer_atoms = list(angle[1])
-        angle_line = (
-            f"{outer_atoms[0]:<2d} {inner_atom:<2d} {outer_atoms[1]:<2d} {angle_type}"
-        )
-        config[" angles "][angle_line] = None
-
-    with open(itp_file, "w") as fp:
-        config.write(fp)
-
-
-def set_dihedrals(itp_file, dihedrals, dihedral_type="3"):
-    config = configparser.ConfigParser(
-        allow_no_value=True, inline_comment_prefixes=["#", ";"]
-    )
-    config.optionxform = str
-    config.read(itp_file)
-
-    config[" dihedrals "] = {}
-    for dihedral in dihedrals:
-        # dihedral is a set of {tuple of (inner_atom, outer_atom), tuple of
-        # (inner_atom, outer_atom)}
-        dihedral_list = list(dihedral)
-        atoms = [
-            dihedral_list[0][1],
-            dihedral_list[0][0],
-            dihedral_list[1][0],
-            dihedral_list[1][1],
-        ]
-        dihedral_line = f"{atoms[0]:<2d} {atoms[1]:<2d} {atoms[2]:<2d} {atoms[3]:<2d} "\
-            f"{dihedral_type}"
-        config[" dihedrals "][dihedral_line] = None
-
-    with open(itp_file, "w") as fp:
-        config.write(fp)
