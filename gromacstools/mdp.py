d16541ef8318cf7423d65b406783b9632a439b80
marvinbernhardt <marvinbernhardt@users.noreply.github.com>
2020-05-06 13:07:18 +0200

    changed mdp.set_parameter to append if key not present

--- gromacstools/mdp.py
+++ gromacstools/mdp.py
@@ -16,11 +16,20 @@ def set_parameter(mdp_file, key, value):
     """sets the value of a parameter in a .mdp file"""
 
     search_string = r"{0}\s*=.*".format(key)
+    search_pattern = re.compile(search_string)
     replacement_string = r"{0} = {1}".format(key, value)
+    changed = False
     with open(mdp_file, "r") as file:
         newlines = []
         for line in file:
-            newlines.append(re.sub(search_string, replacement_string, line))
+            if re.search(search_pattern, line):
+                newlines.append(re.sub(search_string, replacement_string, line))
+                changed = True
+            else:
+                newlines.append(line)
+
+    if not changed:
+        newlines.append(replacement_string + '\n')
 
     with open(mdp_file, "w") as file:
         for line in newlines:
